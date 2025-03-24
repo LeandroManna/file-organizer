cb52b5d1e50f2f2981c42fa057881d69a01d3557
Leandro <leandro.a.manna@gmail.com>
2025-03-24 12:25:52 -0300

    Second commit

--- organizador.py
+++ organizador.py
@@ -1,12 +1,11 @@
 from pathlib import Path
-import shutil
 import logging
 
 # Configuración del logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
 # Ruta de la carpeta Descargas
-carpeta_descargas = Path(r"D:\Documentos")
+carpeta_desorganizada = Path(r"D:\Documentos")
 
 # Diccionario que asocia extensiones con categorías
 categorias_archivos = {
@@ -22,7 +21,7 @@ categorias_archivos = {
 def organizar_descargas():
     """Organiza archivos en la carpeta Descargas según su tipo."""
     
-    for archivo in carpeta_descargas.iterdir():
+    for archivo in carpeta_desorganizada.iterdir():
         if archivo.is_dir():
             continue  # Ignorar carpetas
 
@@ -31,7 +30,7 @@ def organizar_descargas():
 
         for categoria, extensiones in categorias_archivos.items():
             if extension in extensiones:
-                carpeta_destino = carpeta_descargas / categoria
+                carpeta_destino = carpeta_desorganizada / categoria
                 carpeta_destino.mkdir(exist_ok=True)  # Crear si no existe
                 
                 try:
@@ -44,7 +43,7 @@ def organizar_descargas():
 
         # Si no se encontró categoría, mover a 'Otros'
         if not movido:
-            carpeta_otros = carpeta_descargas / 'Otros'
+            carpeta_otros = carpeta_desorganizada / 'Otros'
             carpeta_otros.mkdir(exist_ok=True)
             try:
                 archivo.rename(carpeta_otros / archivo.name)
